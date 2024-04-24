756da64171232b472c13670a5c182edc55372d99
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-04-24 14:28:37 -0300

    removing alloc of str variable

--- src/valid.c
+++ src/valid.c
@@ -2,18 +2,20 @@
 
 static void	check_file(char *file, t_cub *cub)
 {
-	char	*str;
 	int		fd;
 
-	str = ft_substr(file, ft_strlen(file) - 4, 4);
-	if (ft_strcmp(str, ".cub") != 0)
+	if (!ft_strchr(file, '.'))
 	{
 		ft_printf_fd(STDERR_FILENO, "Error\nInvalid file extension\n");
 		free(cub);
-		free(str);
 		exit(EXIT_FAILURE);
 	}
-	free(str);
+	if (ft_strcmp(ft_strrchr(file, '.'), ".cub") != 0)
+	{
+		ft_printf_fd(STDERR_FILENO, "Error\nInvalid file extension\n");
+		free(cub);
+		exit(EXIT_FAILURE);
+	}
 	if ((fd = open(file, O_RDONLY)) == -1)
 	{
 		ft_printf_fd(STDERR_FILENO, "Error\nInvalid file\n");
