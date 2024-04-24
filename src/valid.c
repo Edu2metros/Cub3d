421d3741ef740c814c011a94218dc8a9d1233ab4
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2024-04-24 12:33:27 -0300

    fix erros

--- src/valid.c
+++ src/valid.c
@@ -1,67 +1,97 @@
 #include "../includes/cub3d.h"
 
-static void check_file(char *file, t_cub *cub)
+static void	check_file(char *file, t_cub *cub)
 {
-    char *str;
-    int fd;
-    str = ft_substr(file, ft_strlen(file) - 4, 4);
-    if(ft_strcmp(str, ".cub") != 0)
-    {
-        ft_printf_fd(STDERR_FILENO, "Error\nInvalid file extension\n");
-        free(cub);
-        free(str);
-        exit(EXIT_FAILURE);
-    }
-    free(str);
-    if((fd = open(file, O_RDONLY)) == -1)
-    {
-        ft_printf_fd(STDERR_FILENO, "Error\nInvalid file\n");
-        free(cub);
-        exit(EXIT_FAILURE);
-    }
-    cub->fd = fd;
+	char	*str;
+	int		fd;
+
+	str = ft_substr(file, ft_strlen(file) - 4, 4);
+	if (ft_strcmp(str, ".cub") != 0)
+	{
+		ft_printf_fd(STDERR_FILENO, "Error\nInvalid file extension\n");
+		free(cub);
+		free(str);
+		exit(EXIT_FAILURE);
+	}
+	free(str);
+	if ((fd = open(file, O_RDONLY)) == -1)
+	{
+		ft_printf_fd(STDERR_FILENO, "Error\nInvalid file\n");
+		free(cub);
+		exit(EXIT_FAILURE);
+	}
+	cub->fd = fd;
 }
 
-void extract_lines(t_cub *cub)
+void	exit_program(t_cub *cub)
 {
-	int lines;
-	int i;
+	int	i;
+
+	i = 0;
+	while(cub->file[i])
+	{
+		free(cub->file[i]);
+		i++;
+	}
+	free(cub);
+	exit(EXIT_FAILURE);
+}
+
+void	extract_lines(t_cub *cub)
+{
+	int	lines;
+	int	i;
+
 	lines = get_how_many_lines(cub->fd);
 	cub->file = ft_safe_malloc(sizeof(char *) * (lines + 1));
 	i = 0;
-	while((cub->file[i] = get_next_line(cub->fd)))
+	while ((cub->file[i] = get_next_line(cub->fd)))
 		i++;
 	cub->file[i] = NULL;
 	close(cub->fd);
 }
 
-// void check_texture(t_cub *cub)
-// {
-
-// }
-
-void map_validation(t_cub *cub)
+/* int	check_texture(t_cub *cub)
 {
+	char	**str;
+	int		i;
+	int		i;
+
+	str = {"NO", "SO", "WE", "EA"};
+	i = 0;
+	while (i < 4)
+	{
+		if (ft_strncmp(cub->file[i], str[i], 2) != 0)
+		{
+			ft_printf_fd(STDERR_FILENO, "Error\nInvalid texture\n");
+			free(cub);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	// first 4 lines
+	// check if the file exists
+} */
+void	map_validation(t_cub *cub)
+{
+	// i = 0;
 	extract_lines(cub);
-	// check_texture(cub);
-	//check the texture (first 4 lines)
-	//if the file exists
-
-
-	//check the RGB of the sky and the ground (5 and 6 lines)
-	//check if is in the range (0-255)
-
+	// i = check_texture(cub);
+	// check the texture (first 4 lines)
+	// if the file exists
+	// check the RGB of the sky and the ground (5 and 6 lines)
+	// check if is in the range (0-255)
 	// check the map
 }
 
-void validation(int argc, char **argv, t_cub *cub)
+void	validation(int argc, char **argv, t_cub *cub)
 {
-    if(argc != 2)
-    {
-       ft_printf_fd(STDERR_FILENO, "Error\nInvalid number of arguments\n");
-	   free(cub);
-       exit(EXIT_FAILURE);
-    }
-    check_file(argv[1], cub);
-    //map validation
+	if (argc != 2)
+	{
+		ft_printf_fd(STDERR_FILENO, "Error\nInvalid number of arguments\n");
+		free(cub);
+		exit(EXIT_FAILURE);
+	}
+	check_file(argv[1], cub);
+	// map validation
 }
