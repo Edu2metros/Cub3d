c2da74889f0e5dc9391417f09a95aeefbc577837
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-04-27 14:39:28 -0300

    Treating more than one player and invalid characters in the map

--- src/validate/valid_map.c
+++ src/validate/valid_map.c
@@ -13,6 +13,27 @@ static int ft_strcmp_directions(char *line, char *direction, char **info)
 	return (FALSE);
 }
 
+static int	validate_cel_n_floor(t_cub *cub, char *line)
+{
+	int	i;
+
+	i = 0;
+	if (line[i] == ' ')
+		i++;
+	while (line[i] && line[++i])
+	{
+		if (line[i] == ' ')
+			while (line[i] == ' ')
+				i++;
+		if (!ft_isdigit(line[i]) && line[i] != ',' && line[i] != ' ' && line[i] != '\n')
+		{
+			cub->err_flag = TRUE;
+			return (FALSE);
+		}
+	}
+	return (TRUE);
+}
+
 static int validate_directions(char *line, char **info, t_cub *cub)
 {
 	if (ft_strcmp_directions(line, "NO ", info) == TRUE)
@@ -24,18 +45,114 @@ static int validate_directions(char *line, char **info, t_cub *cub)
 	else if (ft_strcmp_directions(line, "WE ", info) == TRUE)
 		return (TRUE);
 	else if (ft_strcmp_directions(line, "C ", info) == TRUE)
+	{
+		if (!validate_cel_n_floor(cub, line))
+			return (FALSE);
 		return (TRUE);
+	}
 	else if (ft_strcmp_directions(line, "F ", info) == TRUE)
+	{
+		if (!validate_cel_n_floor(cub, line))
+			return (FALSE);
 		return (TRUE);
-    else
-    {
-        cub->err_flag = TRUE;
-        return (FALSE);
-    
-    }
+	}
+	else
+	{
+		cub->err_flag = TRUE;
+		return (FALSE);
+	}
 }
 
-void map_is_valid(t_cub *cub)
+/*
+	param map_start -> ponto inicial onde iniciam-se os vetores
+
+	o loop while roda por todo o mapa, e utiliza a string
+	valid_chars para identificar através da funcao strchr
+	se os caracteres dentro do mapa são todos válidos, caso identifique um caractere que não é esperado (identificado os esperados dentro da string valid_chars) retorna FALSE.
+*/
+
+int	is_valid_characters(char **map_start)
+{
+	int	i;
+	int	j;
+	char valid_chars[9];
+
+	valid_chars[0] = '0';
+	valid_chars[1] = '1';
+	valid_chars[2] = ' ';
+	valid_chars[3] = '\n';
+	valid_chars[4] = 'N';
+	valid_chars[5] = 'S';
+	valid_chars[6] = 'E';
+	valid_chars[7] = 'W';
+	valid_chars[8] = '\0';
+	i = -1;
+	while (map_start[++i])
+	{
+		j = -1;
+		while (map_start[i][++j])
+		{
+			if (!ft_strchr(valid_chars, map_start[i][j]))
+				return (FALSE);
+		}
+	}
+	return (TRUE);
+}
+
+/*
+	param map_start -> ponto inicial onde iniciam-se os vetores
+
+	o loop while roda por todo o mapa, e utiliza a string
+	player_chars para identificar através da funcao strchr
+	quantos players o mapa possuí, se possuir algum valor diferente de 1 que é o esperado, considera erro.
+	(o player pode ser identificado por qualquer um desses caracteres: N, S, E, W)
+*/
+
+int is_more_than_one_player(char **map_start)
+{
+	int	i;
+	int	j;
+	int	player_counter;
+	char player_chars[5];
+
+	player_chars[0] = 'N';
+	player_chars[1] = 'S';
+	player_chars[2] = 'E';
+	player_chars[3] = 'W';
+	player_chars[4] = '\0';
+	i = -1;
+	player_counter = 0;
+	while (map_start[++i])
+	{
+		j = -1;
+		while (map_start[i][++j])
+			if (ft_strchr(player_chars, map_start[i][j]))
+				player_counter++;
+	}
+	printf("players: %i\n", player_counter);
+	if (player_counter != 1)
+		return (TRUE);
+	return (FALSE);
+}
+
+void	validate_map(t_cub *cub, char **map_start)
+{
+	int	i;
+	int	j;
+
+	if (!is_valid_characters(map_start))
+	{
+		cub->err_flag = TRUE;
+		return ;
+	}
+	if (is_more_than_one_player(map_start))
+	{
+		cub->err_flag = TRUE;
+		return ;
+	}
+}
+
+void	map_is_valid(t_cub *cub)
 {
 	int i;
 	int char_pos;
@@ -50,7 +167,10 @@ void map_is_valid(t_cub *cub)
 			continue;
 		char_pos = find_first_char(cub->file[i]);
 		if (ft_isdigit(cub->file[i][char_pos]) == 1)
+		{	
+			validate_map(cub, &cub->file[i]);
 			break;
+		}
 		else
 		    validate_directions(cub->file[i] + char_pos, info, cub);
 	}
